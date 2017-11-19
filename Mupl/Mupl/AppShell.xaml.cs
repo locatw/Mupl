7e9248a6ae6e6790e607edc1147e2210e2b4fb38
locatw <loca@live.jp>
2017-11-19 15:30:36 +0900

    add Prism library and create initial view

---
+++ Mupl/Mupl/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Mupl
+{
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        public void SetContentFrame(Frame frame)
+        {
+            rootSplitView.Content = frame;
+        }
+    }
+}
---
+++ Mupl/Mupl/PageTokens.cs
@@ -0,0 +1,7 @@
+﻿namespace Mupl
+{
+    public enum PageTokens
+    {
+        Main
+    }
+}
